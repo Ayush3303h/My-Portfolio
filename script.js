80bfc018cde95a1d4c68c496f349c53509e333c7
Ayush3303h <ayush.sahu2024@nst.rishihood.edu.in>
2025-02-10 17:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ document.getElementById('hamburger').addEventListener('click', (event) => {
     document.getElementById('navbar').classList.toggle('active');
 });
 
-// Close the navbar when clicking outside of it
+
 document.addEventListener('click', (event) => {
     const navbar = document.getElementById('navbar');
     const hamburger = document.getElementById('hamburger');
     
-    // Check if the click was outside the navbar and hamburger icon
     if (!navbar.contains(event.target) && !hamburger.contains(event.target)) {
         navbar.classList.remove('active');
     }
 });
 
-// Prevent clicks inside the navbar from closing it
+
 document.getElementById('navbar').addEventListener('click', (event) => {
     event.stopPropagation();
 });
